84489095dc1826b319cb19e99409e15ae18bc75a
leslie kimm <leslie.kimm@gmail.com>
2015-11-15 18:21:32 -0800

    Hardcode dummy data to display in UpcomingShowsVC

--- ShowPlanner/ShowPlanner/ViewControllers/UpcomingViewController.swift
+++ ShowPlanner/ShowPlanner/ViewControllers/UpcomingViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 
 class UpcomingViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
     @IBAction func backToUpcomingVC(segue: UIStoryboardSegue) {
         if let identifier = segue.identifier {
             print("Identifier \(identifier)")
         }
     }
+    
+    var shows: [String] = ["The Kevin Nealon Show", "All Star Comedy", "Chocolate Sundaes"]
+    var lineup: [String] = ["Kevin Nealon, Iliza Shlesinger, Bobby Lee, Aries Spears, CJ Sullivan, Chris D'Elia, Sarah Silverman", "Dom Irrera, Mike Marino, Tony Rock, Bob Saget, Godfrey, Kat Williams, Dane Cook, Tim Allen", "Alonzo Bodden, Tiffany Haddish, Aries Spears, Finesse Mitchell, Jerrod Carmichael, Mario Joyner"]
+    var locations: [String] = ["The Laugh Factory", "The Comedy Store", "The Improv"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,4 +42,19 @@ class UpcomingViewController: UIViewController {
     }
     */
 
+
+}
+
+extension UpcomingViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("UpcomingCell") as! ShowTableViewCell
+        cell.showNameLabel.text = self.shows[indexPath.row]
+        cell.lineupLabel.text = self.lineup[indexPath.row]
+        cell.locationLabel.text = self.locations[indexPath.row]
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.shows.count
+    }
 }
