8bb5173529b0c66590864ba7f192cedd481c8466
leslie kimm <leslie.kimm@gmail.com>
2015-11-24 16:01:43 -0800

    Remove cell number and email from ContactTVC

--- ShowPlanner/ShowPlanner/Views/ContactTableViewCell.swift
+++ ShowPlanner/ShowPlanner/Views/ContactTableViewCell.swift
@@ -10,26 +10,12 @@ import UIKit
 
 class ContactTableViewCell: UITableViewCell {
     @IBOutlet weak var contactNameLabel: UILabel!
-    @IBOutlet weak var contactEmailLabel: UILabel!
-    @IBOutlet weak var contactCellLabel: UILabel!
-    
+
     var contact: Contact? {
         didSet {
-            if let contact = contact, contactNameLabel = contactNameLabel, contactEmailLabel = contactEmailLabel, contactCellLabel = contactCellLabel {
+            if let contact = contact, contactNameLabel = contactNameLabel {
                 contactNameLabel.text = contact.name
-                contactEmailLabel.text = contact.email
-                contactCellLabel.text = contact.cell
             }
         }
-    }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-        // Configure the view for the selected state
     }
 }
