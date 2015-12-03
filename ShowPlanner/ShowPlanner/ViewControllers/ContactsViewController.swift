0444c178dd627f15ae21f8cd10f1bb10ee1d458d
leslie kimm <leslie.kimm@gmail.com>
2015-12-02 21:12:56 -0800

    Rename ContactsV2VC to ContactsVC

--- ShowPlanner/ShowPlanner/ViewControllers/ContactsViewController.swift
+++ ShowPlanner/ShowPlanner/ViewControllers/ContactsViewController.swift
@@ -1,54 +1,34 @@
 //
 //  ContactsViewController.swift
-//  ShowPlanner
+//  Show Planner
 //
-//  Created by Leslie Kim on 11/19/15.
+//  Created by Leslie Kim on 12/2/15.
 //  Copyright © 2015 Leslie Kim. All rights reserved.
 //
 
-// SOURCES: 1) Import contacts using Contacts framework
-//             http://www.appcoda.com/ios-contacts-framework/
-
 import UIKit
-import RealmSwift
-import Contacts
 
 class ContactsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, ImportContactViewControllerDelegate {
     @IBOutlet weak var contactsTableView: UITableView!
     
-    var contacts: Results<Contact>! {
-        didSet {
-            contactsTableView?.reloadData()
-        }
-    }
+    var dataSource = ContactsDataSource()
     var selectedContact: Contact?
     
     @IBAction func backToContactsVC(segue: UIStoryboardSegue) {
         if let identifier = segue.identifier {
-            do {
-                let realm = try Realm()
-                
-                switch identifier {
-                case "contactSaveSegue":
-                    let source = segue.sourceViewController as! AddContactViewController
-                    try realm.write() {
-                        realm.add(source.currentContact!)
-                    }
-                
-                case "trashContactSegue":   // not needed for case where adding new contact - causes error FIX
-                    try realm.write() {
-                        realm.delete(self.selectedContact!)
-                    }
-                    let source = segue.sourceViewController as! ContactDisplayViewController
-                    source.contact = nil
-                default:
-                    print("No one loves \(identifier)")
-                }
-                
-                contacts = realm.objects(Contact).sorted("name", ascending: true)
-            }
-            catch {
-                print("Error in backToContactsVC")
+            switch identifier {
+            case "contactSaveSegue":
+                let source = segue.sourceViewController as! AddContactViewController
+                let contactToAdd = source.currentContact!
+                dataSource.addContact(contactToAdd)
+                contactsTableView.reloadData()
+            case "trashContactSegue":
+                let source = segue.sourceViewController as! ContactDisplayViewController
+                dataSource.trashContact(selectedContact!)
+                source.contact = nil
+                contactsTableView.reloadData()
+            default:
+                print("No one loves \(identifier)")
             }
         }
     }
@@ -57,18 +37,12 @@ class ContactsViewController: UIViewController, UITableViewDataSource, UITableVi
         super.viewDidLoad()
         contactsTableView.dataSource = self
         contactsTableView.delegate = self
+        contactsTableView.reloadData()
     }
     
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        do {
-            let realm = try Realm()
-            contacts = realm.objects(Contact).sorted("name", ascending: true)
-        }
-        catch {
-            print("Error in Contacts viewDidLoad")
-        }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        contactsTableView.reloadData()
     }
     
     // MARK: Navigation
@@ -90,18 +64,26 @@ class ContactsViewController: UIViewController, UITableViewDataSource, UITableVi
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ContactCell") as! ContactTableViewCell
         let row = indexPath.row
-        let contact = contacts[row] as Contact
+        let contact = dataSource.contacts[row] as Contact
         cell.contact = contact
         return cell
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return contacts?.count ?? 0
+        return dataSource.contacts?.count ?? 0
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            let contact = dataSource.contacts[indexPath.row] as Contact
+            dataSource.trashContact(contact)
+            tableView.reloadData()
+        }
     }
     
     // MARK: UITableViewDelegate
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        selectedContact = contacts[indexPath.row]
+        selectedContact = dataSource.contacts[indexPath.row]
         self.performSegueWithIdentifier("showExistingContactSegue", sender: self)
     }
     
@@ -109,36 +91,10 @@ class ContactsViewController: UIViewController, UITableViewDataSource, UITableVi
         return true
     }
     
-    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            let contact = contacts[indexPath.row] as Object
-            
-            do {
-                let realm = try Realm()
-                try realm.write() {
-                    realm.delete(contact)
-                }
-                contacts = realm.objects(Contact).sorted("name", ascending: true)
-            }
-            catch {
-                print("ERROR")
-            }
-        }
-    }
-    
     // MARK: ImportContactViewControllerDelegate
     func didFetchContacts(contactsToAppend: [Contact]) {
         for contact in contactsToAppend {
-            do {
-                let realm = try Realm()
-                try realm.write() {
-                    realm.add(contact)
-                }
-                contacts = realm.objects(Contact).sorted("name", ascending: true)
-            }
-            catch {
-                print("Error in didFetchContacts")
-            }
+            dataSource.addContact(contact)
         }
         
         contactsTableView.reloadData()
