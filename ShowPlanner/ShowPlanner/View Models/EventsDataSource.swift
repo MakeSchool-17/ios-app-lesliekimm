38359e7fbc53d6fd4983070a6dbda6c3ac6b19ed
leslie kimm <leslie.kimm@gmail.com>
2015-12-08 08:43:47 -0800

    Comment EventsDataSource

--- ShowPlanner/ShowPlanner/View Models/EventsDataSource.swift
+++ ShowPlanner/ShowPlanner/View Models/EventsDataSource.swift
@@ -10,44 +10,47 @@ import Foundation
 import RealmSwift
 
 class EventsDataSource: NSObject {
-    static var sharedEventsDatsSource = EventsDataSource()
-    var events: Results<Event>!
+    static var sharedEventsDatsSource = EventsDataSource()      // use static var to create singleton
+    var events: Results<Event>!                                 // declare events var
     
+    // Access Realm and sort events by date and time
     override init() {
         super.init()
         
         do {
-            let realm = try Realm()
-            events = realm.objects(Event).sorted("dateTime", ascending: true)
+            let realm = try Realm()                                             // grab default Realm
+            events = realm.objects(Event).sorted("dateTime", ascending: true)   // sort by date and time
         }
         catch {
-            print("Error in events init")
+            print("Error in events init")                                       // print error message
         }
     }
     
+    // Add an event to events
     func addEvent(event: Event) {
         do {
-            let realm = try Realm()
-            try realm.write() {
-                realm.add(event)
+            let realm = try Realm()                                             // grab default Realm
+            try realm.write() {                                                 // write to Realm
+                realm.add(event)                                                // add event to Realm
             }
-            events = realm.objects(Event).sorted("dateTime", ascending: true)
+            events = realm.objects(Event).sorted("dateTime", ascending: true)   // sort by date and time
         }
         catch {
-            print("Error in addEvent")
+            print("Error in addEvent")                                          // print error message
         }
     }
     
+    // Delete an event from events
     func trashEvent(event: Event) {
         do {
-            let realm = try Realm()
-            try realm.write() {
-                realm.delete(event)
+            let realm = try Realm()                                             // grab default Realm
+            try realm.write() {                                                 // write to Realm
+                realm.delete(event)                                             // delete event from Realm
             }
-            events = realm.objects(Event).sorted("dateTime", ascending: true)
+            events = realm.objects(Event).sorted("dateTime", ascending: true)   // sort by date and time
         }
         catch {
-            print("Error in trashEvent")
+            print("Error in trashEvent")                                        // print error message
         }
     }
 }
