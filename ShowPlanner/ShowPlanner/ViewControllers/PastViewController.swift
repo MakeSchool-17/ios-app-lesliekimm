3b05b2fdd3816a9f91535f5b38bd554fed4ce789
leslie kimm <leslie.kimm@gmail.com>
2015-12-08 08:16:21 -0800

    Comment PastVC & PastTVC

--- ShowPlanner/ShowPlanner/ViewControllers/PastViewController.swift
+++ ShowPlanner/ShowPlanner/ViewControllers/PastViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class PastViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    @IBOutlet weak var pastTableView: UITableView!
+    @IBOutlet weak var pastTableView: UITableView!              // code connection to PastShows TV
+    var dataSource = EventsDataSource()                         // reference to EventsDataSource
+    var selectedEvent: Event?                                   // selected event
+    var eventsToBeDisplayed: [Event]?                           // array of events to display on PastVC
     
-    var dataSource = EventsDataSource()
-    var selectedEvent: Event?
-    var eventsToBeDisplayed: [Event]?
-    
+    // Set dataSource and delegate to self, reload data and don't allow cells to be selected
     override func viewDidLoad() {
         super.viewDidLoad()
         pastTableView.dataSource = self
@@ -23,38 +23,44 @@ class PastViewController: UIViewController, UITableViewDataSource, UITableViewDe
         pastTableView.allowsSelection = false
     }
     
+    // Each time view appears, initialize eventsToBeDisplayed and populate array with events that have
+    // already happened
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        eventsToBeDisplayed = [Event]()
-        let currentTime = NSDate()
+        
+        eventsToBeDisplayed = [Event]()                         // initialize array
+        let currentTime = NSDate()                              // get current time
+        
+        // For each event in EventsDataSource, compare to current time and if event has passed, insert
+        // into the beginnig of the array so table view gets populated from most recent event to oldest
         for event in dataSource.events {
             if event.dateTime.compare(currentTime) ==  NSComparisonResult.OrderedAscending {
                 eventsToBeDisplayed?.insert(event, atIndex: 0)
             }
         }
-        pastTableView.reloadData()
+        pastTableView.reloadData()                              // reload data
     }
     
     // MARK: UITableViewDataSource
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Get each cell of pastTableView and populate with events in eventsToBeDisplayed array
         let cell = tableView.dequeueReusableCellWithIdentifier("PastEventCell") as! PastTableViewCell
-        let row = indexPath.row
-        let event = (eventsToBeDisplayed?[row])! as Event
+        let event = (eventsToBeDisplayed?[indexPath.row])! as Event
         cell.event = event
         return cell
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return eventsToBeDisplayed?.count ?? 0
+        return eventsToBeDisplayed?.count ?? 0                  // TV rows equals eventsToBeDisplayed count
     }
     
     // MARK: UITableViewDelegate
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        selectedEvent = eventsToBeDisplayed![indexPath.row]
-        self.performSegueWithIdentifier("showPastEvent", sender: self)
+        selectedEvent = eventsToBeDisplayed![indexPath.row]             // set selectedEvent
+        self.performSegueWithIdentifier("showPastEvent", sender: self)  // show PastNotesVC for selectedEvent
     }
     
     func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
+        return true                                             // rows can be edited
     }
 }
--- ShowPlanner/ShowPlanner/Views/PastTableViewCell.swift
+++ ShowPlanner/ShowPlanner/Views/PastTableViewCell.swift
@@ -9,28 +9,32 @@
 import UIKit
 
 class PastTableViewCell: UITableViewCell {
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var eventNameLabel: UILabel!             // code connection for event name label
+    @IBOutlet weak var locationLabel: UILabel!              // code connection for location label
+    @IBOutlet weak var dateLabel: UILabel!                  // code connection for date label
+    @IBOutlet weak var timeLabel: UILabel!                  // code connection for time label
     
+    // Format appearance of dateLabel
     static var dateFormatter: NSDateFormatter = {
-        var formatter = NSDateFormatter()
-        formatter.dateStyle = .MediumStyle
-        return formatter
+        var formatter = NSDateFormatter()                   // declare NSDateFormatter object
+        formatter.dateStyle = .MediumStyle                  // use MediumStyle for date
+        return formatter                                    // return NSDateFormatter object
     }()
     
+    // Formate appearance of timeLabel
     static var timeFormatter: NSDateFormatter = {
-        var formatter = NSDateFormatter()
-        formatter.timeStyle = .ShortStyle
-        return formatter
+        var formatter = NSDateFormatter()                   // declare NSDateFormatter object
+        formatter.timeStyle = .ShortStyle                   // use ShortStyle for time
+        return formatter                                    // return NSDateFormatter object
     }()
     
+    // Create event var and use didSet method to keep event updated upon changes
     var event: Event? {
         didSet {
-            if let event = event, nameLabel = nameLabel, locationLabel = locationLabel, dateLabel = dateLabel, timeLabel = timeLabel {
-                nameLabel.text = event.name
-                locationLabel.text = event.location
+            if let event = event, eventNameLabel = eventNameLabel, locationLabel = locationLabel, dateLabel = dateLabel, timeLabel = timeLabel {
+                eventNameLabel.text = event.name            // set eventNameLabel to event name
+                locationLabel.text = event.location         // set locationLabel to event location
+                // Set dateLabel and timeLabel to event date and time formatted using declared styles
                 dateLabel.text = EventTableViewCell.dateFormatter.stringFromDate(event.dateTime)
                 timeLabel.text = EventTableViewCell.timeFormatter.stringFromDate(event.dateTime)
             }
