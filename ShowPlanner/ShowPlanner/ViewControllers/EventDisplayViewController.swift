1649bd6abeba789080c78661b20b7e07db86710f
leslie kimm <leslie.kimm@gmail.com>
2015-12-09 10:47:19 -0800

    Add tapgesture to EventDisplay

--- ShowPlanner/ShowPlanner/ViewControllers/EventDisplayViewController.swift
+++ ShowPlanner/ShowPlanner/ViewControllers/EventDisplayViewController.swift
@@ -5,6 +5,8 @@
 //  Created by Leslie Kim on 11/22/15.
 //  Copyright © 2015 Leslie Kim. All rights reserved.
 //
+// SOURCES:
+// 1) Dismiss keyboard with UITapGestureRecognizer: http://stackoverflow.com/questions/24126678/close-ios-keyboard-by-touching-anywhere-using-swift
 
 import Foundation
 import UIKit
@@ -50,6 +52,14 @@ class EventDisplayViewController: UIViewController, UITableViewDataSource, UITab
     var selectedLineup: Lineup?
     var stringLineup: [String] = []
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Initialize UITapGestureRecognizer
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)                      // add tap gesture to view
+    }
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         self.tabBarController?.tabBar.hidden = true
@@ -72,6 +82,11 @@ class EventDisplayViewController: UIViewController, UITableViewDataSource, UITab
         super.viewWillDisappear(animated)
         self.tabBarController?.tabBar.hidden = false
 //        saveEvent()
+    }
+    
+    // View will resign first responder status
+    func dismissKeyboard() {
+        view.endEditing(true)                               // dismiss keyboard
     }
     
     func displayEvent(event: Event?) {
