eeaa6eee29cf6e058e096a3a4a09ae0243ef41ef
leslie kimm <leslie.kimm@gmail.com>
2015-12-11 12:02:18 -0800

    Remove TextField search from ImportVC

--- ShowPlanner/ShowPlanner/ViewControllers/ImportViewController.swift
+++ ShowPlanner/ShowPlanner/ViewControllers/ImportViewController.swift
@@ -15,8 +15,8 @@ protocol ImportContactViewControllerDelegate {
     func didFetchCNContacts(contacts: [CNContact])
 }
 
-class ImportViewController: UIViewController, UITextFieldDelegate, CNContactPickerDelegate {
-    @IBOutlet weak var searchTextField: UITextField!
+class ImportViewController: UIViewController, CNContactPickerDelegate {
+//    @IBOutlet weak var searchTextField: UITextField!
     
     var delegate: ImportContactViewControllerDelegate!
     var contacts = [CNContact]()
@@ -34,7 +34,7 @@ class ImportViewController: UIViewController, UITextFieldDelegate, CNContactPick
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        searchTextField.delegate = self
+//        searchTextField.delegate = self
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -48,64 +48,64 @@ class ImportViewController: UIViewController, UITextFieldDelegate, CNContactPick
     }
     
     // MARK: UITextFieldDelegate
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        AppDelegate.getAppDelegate().requestForAccess { (accessGranted) -> Void in
-            if accessGranted {
-                let predicate = CNContact.predicateForContactsMatchingName(self.searchTextField.text!)
-                let keys = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactEmailAddressesKey, CNContactPhoneNumbersKey]
-                var contacts = [CNContact]()
-                var message: String!
-                
-                let contactsStore = AppDelegate.getAppDelegate().contactStore
-                do {
-                    contacts = try contactsStore.unifiedContactsMatchingPredicate(predicate, keysToFetch: keys)
-                    
-                    if contacts.count == 0 {
-                        message = "No contacts found matching search criteria."
-                    }
-                }
-                catch {
-                    message = "Unable to fetch contacts."
-                }
-                
-                if message != nil {
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        AppDelegate.getAppDelegate().showMessage(message)
-                    })
-                }
-                else {
-                    for contact in contacts {
-                        let newContact = Contact()
-                        
-                        if contact.givenName != "" && contact.familyName != "" {
-                            newContact.name = "\(contact.givenName) \(contact.familyName)"
-                        }
-                        else if contact.givenName != "" {
-                            newContact.name = "\(contact.givenName)"
-                        }
-                        else {
-                            newContact.name = "\(contact.familyName)"
-                        }
-                        
-                        if !contact.phoneNumbers.isEmpty {
-                            newContact.cell = (contact.phoneNumbers[0].value as! CNPhoneNumber).stringValue
-                        }
-                        if !contact.emailAddresses.isEmpty {
-                            newContact.email = contact.emailAddresses[0].value as! String
-                        }
-            
-                        self.contactsToAppend.append(newContact)
-                    }
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        self.delegate.didFetchContacts(self.contactsToAppend)
-                        self.delegate.didFetchCNContacts(contacts)
-                        self.navigationController?.popViewControllerAnimated(true)
-                    })
-                }
-            }
-        }
-        return true
-    }
+//    func textFieldShouldReturn(textField: UITextField) -> Bool {
+//        AppDelegate.getAppDelegate().requestForAccess { (accessGranted) -> Void in
+//            if accessGranted {
+//                let predicate = CNContact.predicateForContactsMatchingName(self.searchTextField.text!)
+//                let keys = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactEmailAddressesKey, CNContactPhoneNumbersKey]
+//                var contacts = [CNContact]()
+//                var message: String!
+//                
+//                let contactsStore = AppDelegate.getAppDelegate().contactStore
+//                do {
+//                    contacts = try contactsStore.unifiedContactsMatchingPredicate(predicate, keysToFetch: keys)
+//                    
+//                    if contacts.count == 0 {
+//                        message = "No contacts found matching search criteria."
+//                    }
+//                }
+//                catch {
+//                    message = "Unable to fetch contacts."
+//                }
+//                
+//                if message != nil {
+//                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                        AppDelegate.getAppDelegate().showMessage(message)
+//                    })
+//                }
+//                else {
+//                    for contact in contacts {
+//                        let newContact = Contact()
+//                        
+//                        if contact.givenName != "" && contact.familyName != "" {
+//                            newContact.name = "\(contact.givenName) \(contact.familyName)"
+//                        }
+//                        else if contact.givenName != "" {
+//                            newContact.name = "\(contact.givenName)"
+//                        }
+//                        else {
+//                            newContact.name = "\(contact.familyName)"
+//                        }
+//                        
+//                        if !contact.phoneNumbers.isEmpty {
+//                            newContact.cell = (contact.phoneNumbers[0].value as! CNPhoneNumber).stringValue
+//                        }
+//                        if !contact.emailAddresses.isEmpty {
+//                            newContact.email = contact.emailAddresses[0].value as! String
+//                        }
+//            
+//                        self.contactsToAppend.append(newContact)
+//                    }
+//                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                        self.delegate.didFetchContacts(self.contactsToAppend)
+//                        self.delegate.didFetchCNContacts(contacts)
+//                        self.navigationController?.popViewControllerAnimated(true)
+//                    })
+//                }
+//            }
+//        }
+//        return true
+//    }
     
     // MARK: CNContactPickerDelegate
     func contactPicker(picker: CNContactPickerViewController, didSelectContacts contacts: [CNContact]) {
